3f4473435116c9abfb06d2058f2bfcaf47392404
KonstantinKhamilov <134136455+KonstantinKhamilov@users.noreply.github.com>
2024-08-04 01:50:00 +0300

    замена редюсера в стор. код работает без ошибок. требуется доп проверка ментора

--- src/redux/contactsSlice.jsx
+++ src/redux/contactsSlice.jsx
@@ -1,16 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState = {
+  items: [],
+};
 
 const contactsSlice = createSlice({
   name: "contacts",
   initialState,
   reducers: {
     addContact(state, action) {
-      state.push(action.payload);
+      state.items.push(action.payload);
+    },
+    deleteContact(state, action) {
+      state.items = state.items.filter(
+        (contact) => contact.id !== action.payload
+      );
     },
   },
 });
 
-export const { addContact } = contactsSlice.actions;
+export const { addContact, deleteContact } = contactsSlice.actions;
+
+export const selectContacts = (state) => state.contacts.items;
+
 export default contactsSlice.reducer;
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,11 +1,22 @@
-// store.js
 import { createStore, combineReducers } from "redux";
-import contactsReducer from "./contactsReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import contactsReducer from "./contactsSlice";
+
+const persistConfig = {
+  key: "contacts",
+  storage,
+  whitelist: ["items"],
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
 
 const rootReducer = combineReducers({
-  contacts: contactsReducer,
+  contacts: persistedContactsReducer,
 });
 
 const store = createStore(rootReducer);
+const persistor = persistStore(store);
 
+export { store, persistor };
 export default store;
